f70d9d80de8a414891b2753b3282092f38d64980
CodyBanks <codydouglasbanks@gmail.com>
2020-06-23 16:50:39 -0400

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,54 @@ import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
+  state = {
+    hue: 0,
+    saturation: 0,
+    lightness: 0,
+  }
   render() {
-    return <HelloWorld />
+    return (
+      <body>
+        <main>
+          <section>
+            <h2>Color</h2>
+            <div style={{ backgroundColor: currentColor }}></div>
+          </section>
+          <section>
+            <article>
+              <h2>H</h2>
+              <input
+                type="range"
+                min="0"
+                max="255"
+                value=""
+                onChange=""
+              ></input>
+            </article>
+            <article>
+              <h2>S</h2>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value=""
+                onChange=""
+              ></input>
+            </article>
+            <article>
+              <h2>L</h2>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value=""
+                onChange=""
+              ></input>
+            </article>
+          </section>
+        </main>
+      </body>
+    )
   }
 }
 
